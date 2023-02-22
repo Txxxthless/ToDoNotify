6e2d011c4c0236ecc3e4373086c16ad4294482c2
Txxxthless <lepash85@gmail.com>
2023-02-22 10:44:30 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace ToDoNotify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToDoNotify_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
